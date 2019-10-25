daf4fbefc241a4128991f492d724ac9b5185a5fc
hayoung98 <jaskk870506@gmail.com>
2019-10-25 14:56:40 +0800

    加註解

--- test/main.py
+++ test/main.py
@@ -20,21 +20,21 @@ def main():
         if postfix_r[i] in "01":
             w.append(postfix_r[i])
         if postfix_r[i] == '*':
-            F = []
+            F = [] # 存output的list
             zero = ''.join(random.sample(char_list,len_of_F))  # 隨機產生字串
             for i in range(3):
-                F.append(zero)
+                F.append(zero) # ANDgate前面3個是0
             one = ''.join(random.sample(char_list,len_of_F))
-            F.append(one)
+            F.append(one) # 最後一個是1
             # print(F)
             gatename = "AND"
         elif postfix_r[i] == '+':
             F = []
             zero = ''.join(random.sample(char_list, len_of_F))
-            F.append(zero)
+            F.append(zero) # ORgate第一個是0
             one = ''.join(random.sample(char_list, len_of_F))
             for i in range(3):
-                F.append(one)
+                F.append(one) # ORgate後面三個是1
             # print(F)
             gatename = "OR"
         '''
@@ -49,8 +49,8 @@ def main():
         '''
 
 
-    A = []
-    B = []
+    A = [] # 存inputA
+    B = [] # 存inputB
     # 產生input(A0,A1,B0,B1)
     for i in range(2):
         A.append(''.join(random.sample(char_list,3)))  # 隨機產生3個字元的字串
@@ -59,13 +59,13 @@ def main():
     print('B0=', B[0],'  B1=', B[1])
     print('F0=', zero,'  F1=',one)
     truth_table = []
-    c=0
+    c=0 # outputF的counter
     for i in range(2):
         for j in range(2):
             combine = []
             combine.append(A[i])
             combine.append(B[j])
-            combine.append(F[c])
+            combine.append(F[c]) # 產生truth table的每一排
             truth_table.append(combine)
             c=c+1
 
@@ -76,9 +76,9 @@ def main():
     '''
     w = input("\nplease input AB (ex.00,01,10,11): ")
     '''
-    w_num = []
-    w_num.append(int(w[0]))
-    w_num.append(int(w[1]))
+    w_num = [] # 存運算元轉int
+    w_num.append(int(w[0])) # inputA轉成int並存入
+    w_num.append(int(w[1])) # inputB轉成int並存入
     print(F[w_num[0]*2+w_num[1]],'\n↓')
     enigma.main(A[w_num[0]],B[w_num[1]],F[w_num[0]*2+w_num[1]])
 
