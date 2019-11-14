b42598ddae76409b64298164b527f1c0a022fc57
xian797516 <51256558+xian797516@users.noreply.github.com>
2019-11-14 13:40:21 +0800

    Update xor_hash.py

--- test/xor_hash.py
+++ test/xor_hash.py
@@ -3,11 +3,11 @@ import hashlib
 def x_h(sign_truth_table):
     encrypt_table = []
     for table_len in range(len(sign_truth_table)):
-        sha = hashlib.md5()
-        s1_t = int(sign_truth_table[table_len][0], 0)
-        s2_t = int(sign_truth_table[table_len][1], 0)
-        s3_t = int(sign_truth_table[table_len][2], 0)
-        x_result = str(hex(s1_t ^ s2_t ^ s3_t))
+        sha = hashlib.sha256()
+        s1_t = int(sign_truth_table[table_len][0], 16)
+        s2_t = int(sign_truth_table[table_len][1], 16)
+        s3_t = int(sign_truth_table[table_len][2], 16)
+        x_result = hex(s1_t ^ s2_t ^ s3_t)[2:]
         sha_data = sign_truth_table[table_len][0] + sign_truth_table[table_len][1] + sign_truth_table[table_len][2]
         sha.update(sha_data.encode("utf-8"))
         sha_result = sha.hexdigest()#hash value
