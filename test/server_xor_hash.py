34ee8c2c90a11581aaa2a38d97667c91667e8eaa
xian797516 <51256558+xian797516@users.noreply.github.com>
2019-11-14 13:41:22 +0800

    Update server_xor_hash.py

--- test/server_xor_hash.py
+++ test/server_xor_hash.py
@@ -3,10 +3,10 @@ import hashlib
 def s_x_h(input_A, input_B, encrypt_table):
     for table_len in range(len(encrypt_table)):
         sha = hashlib.md5()
-        s1_t = int(input_A, 0)
-        s2_t = int(input_B, 0)
-        s3_t = int(encrypt_table[table_len][0], 0)
-        x_result = str(hex(s1_t ^ s2_t ^ s3_t))
+        s1_t = int(input_A, 16)
+        s2_t = int(input_B, 16)
+        s3_t = int(encrypt_table[table_len][0], 16)
+        x_result = hex(s1_t ^ s2_t ^ s3_t)[2:]
         sha_data = input_A + input_B + x_result
         sha.update(sha_data.encode("utf-8"))
         sha_result = sha.hexdigest()#hash value
